2e93b85036ee322d32add718aa1c32d2acc42430
Francis <super_f_qc@hotmail.com>
2015-12-21 01:11:53 -0500

    bootbox ces a chier

---
+++ OpenCocktail/Scripts/AboutBootBox.js
@@ -0,0 +1,16 @@
+﻿/*function aboutBootbox()
+{
+    bootbox.dialog({
+        message: "Site réaliser par Francis Lemaire et Xavier Brosseau dans le cadre du cours 420-KE9 au Collège Lionel-Groulx, selon les directive de Nicolas Chourot.",
+        title: "About",
+        buttons: {
+            main: {
+                label: "Click ME!",
+                className: "btn-primary",
+                callback: function () {
+                    Example.show("Primary button");
+                }
+            }
+        }
+    });
+}*/
